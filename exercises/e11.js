28a7ea3c1983df852886ca6db48cbbe99cab22d7
solidocean007 <clintonwilliams007@gmail.com>
2023-06-14 20:39:47 -0400

    Complete e-11

--- exercises/e11.js
+++ exercises/e11.js
@@ -12,7 +12,7 @@
  * * Import fetch function from 'node-fetch' to use the fetch() function in code
  * * set the usersUrl constant to store the json-server 'users' endpoint path
 */
-
+import fetch from "node-fetch";
 export const usersUrl = 'http://localhost:3000/users/';
 
 /**
@@ -26,10 +26,11 @@ export const usersUrl = 'http://localhost:3000/users/';
  * Example: const getLoginList = (data) => {<Your code>}
 */
 
-const getLoginList = () => {
-  // Your code goes here...
-
+const getLoginList = (data) => {
+  return data.map(item => item.login);
 }
+
+
 
 /**
  * @task 
@@ -39,7 +40,7 @@ const getLoginList = () => {
 */
 
 // Your code goes here ...
-const getData;
+const getData = fetch(usersUrl);
 
 /**
  * @task 
@@ -53,7 +54,14 @@ const getData;
 */
 
 // Your code goes here ...
-export const result = getData;
+export const result = getData
+  .then(response => response.json())
+  .then(data => {
+    const logins = getLoginList(data);
+    console.log(logins);
+    return logins;
+  })
+  .catch(error => console.error(error));
 
 
 // === TEST YOURSELF ===
